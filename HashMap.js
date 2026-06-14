f4972e90093506c0a2dc070bb6cca7148b054369
prashanthans02 <prashanthanand873@gmail.com>
2026-06-14 23:42:05 +0530

    Finishing the Project.

--- HashMap.js
+++ HashMap.js
@@ -205,15 +205,24 @@ function HashMap() {
 
         return entriesArr;
     };
-
-    function getCapacity() {
-        return `capacity: ${capacity}, length: ${buckets.length} entires: ${size}`;
-    }
     
     return {
-        set, get, has, remove, length, clear, keys, values, entries, getCapacity
+        set, get, has, remove, length, clear, keys, values, entries
     }
 };
 
-
+let test = HashMap();
+test.set('apple', 'red');
+test.set('banana', 'yellow');
+test.set('carrot', 'orange');
+test.set('dog', 'brown');
+test.set('elephant', 'gray');
+test.set('frog', 'green');
+test.set('grape', 'purple');
+test.set('hat', 'black');
+test.set('ice cream', 'white');
+test.set('jacket', 'blue');
+test.set('kite', 'pink');
+test.set('lion', 'golden');
+test.set('moon', 'silver');
 
